ce1515b98e5405257e33d80e3a953a681e693c21
Tony <cz9x@qq.com>
2016-07-13 19:29:09 +0800

    beautifylsoup

---
+++ piaoniu.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+import urllib2
+from bs4 import BeautifulSoup
+
+class PiaoNiu:
+    def __init__(self, baseUrl):
+        self.baseUrl = baseUrl
+
+
+    def getPage(self):
+        try:
+            url = self.baseUrl
+            request = urllib2.Request(url)
+            response = urllib2.urlopen(request)
+            return response.read().decode('utf-8')
+        except urllib2.URLError, e:
+            if hasattr(e, "reason"):
+                print u"连接失败,错误原因: ", e.reason
+                return None
+
+
+    def getContent(self, Page):
+        soup = BeautifulSoup(Page, "html.parser")
+        print soup.find_all("li", class_="item")
+
+
+
+baseUrl = 'http://www.piaoniu.com/sh-concerts'
+piaoniu = PiaoNiu(baseUrl)
+
+print piaoniu.getContent(piaoniu.getPage())
+
